d53206d4304e9c9e2458de5d5286259ca4959899
ori davidi <oridavidi100@gmail.com>
2021-11-01 15:57:08 +0200

    deployment

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors= require('cors');
+
+const Pokedex = require('pokedex-promise-v2');
+const P = new Pokedex();
+app.use(cors({
+  origin:'*'
+}))
+app.use(express.static('public'));
+app.use(express.json()) // parses requests as json
+
+const pokemonRouter = require("./src/routers/pokemonRouter");
+const userRouter = require("./src/routers/userRouter");
+const {errorHandlerMiddleware} = require("./src/middleware/errorHandler")
+const {userHandler} = require("./src/middleware/userHandler");
+
+
+app.use(userHandler);
+
+app.use('/pokemon', pokemonRouter);
+app.use('/info', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(errorHandlerMiddleware);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
